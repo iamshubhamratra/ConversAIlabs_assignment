eb50b2aa4e98481db514e3dcfeb1fb8dd8ff5190
iamshubhamratra <ratrashubham129@gmail.com>
2026-02-13 14:02:56 +0530

    assignment completed

---
+++ utils/tokenGenerator.js
@@ -0,0 +1,19 @@
+const crypto = require("crypto");
+const { getSecretFromDB } = require("./mockDb");
+
+const generateToken = async (email) => {
+  try {
+    const secret = await getSecretFromDB();
+
+    return crypto
+      .createHmac("sha256", secret)
+      .update(email)
+      .digest("base64");
+  } catch (error) {
+    // THE BUG: Empty catch block.
+    // Error is swallowed and undefined is returned.
+    throw new Error("Error generating token");
+  }
+};
+
+module.exports = { generateToken };
